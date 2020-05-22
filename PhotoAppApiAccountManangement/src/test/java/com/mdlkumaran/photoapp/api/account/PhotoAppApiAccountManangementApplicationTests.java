b8c06a39c5b64a68e591c76254049c073b79748e
Kumaran MDL <mdlkumaran@gmail.com>
2020-05-22 05:28:40 -0700

    Spring Microservices-final

---
+++ PhotoAppApiAccountManangement/src/test/java/com/mdlkumaran/photoapp/api/account/PhotoAppApiAccountManangementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdlkumaran.photoapp.api.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiAccountManangementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
